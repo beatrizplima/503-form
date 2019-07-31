1e19c80819e6c5c05b617d2eec566e350def6cee
beatrizplima <52544576+beatrizplima@users.noreply.github.com>
2019-07-31 18:03:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let nome = document.querySelector("#nome");
+let email = document.querySelectorAll("#email");
+let email2 = document.querySelector("#email2")
+let cadastrar = document.querySelector("h1");
+let sexo = document.querySelectorAll("select");
+let resposta = document.querySelectorAll("p");
+
+if(email = email2){
+    resposta.innerHTML
+}
